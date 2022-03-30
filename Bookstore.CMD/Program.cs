20450fb4729c7d04ae66fa7485cd49d53258d3b5
ArtBuz <buzinovart@yandex.ru>
2022-03-30 18:04:58 +0300

    add CMD project

---
+++ Bookstore.CMD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bookstore.CMD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
