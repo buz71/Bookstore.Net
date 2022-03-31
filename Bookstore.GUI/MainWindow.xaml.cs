fbf79c1d71c05cf39825cf85858dcb9c0a4e9c2f
ArtBuz <buzinovart@yandex.ru>
2022-03-31 19:06:46 +0300

    connect Autorization method to GUI

--- Bookstore.GUI/MainWindow.xaml.cs
+++ Bookstore.GUI/MainWindow.xaml.cs
@@ -12,14 +12,19 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Bookstore.MControl;
+using Bookstrore.MControl.Control;
+using Microsoft.Data.Sqlite;
 
 namespace Bookstore.GUI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Переменная которая хравнит объект подключения для работы с БД.
+        /// В случае успешной авторизации, метод Autorization возвращает объект подключения к БД
+        /// </summary>
+        private BookstoreDb db; 
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +32,17 @@ namespace Bookstore.GUI
 
         private void Button_login_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            try
+            {
+                //Для тестирования можно попробовать login:admin, password:admin 
+                db=AccountManager.Autorization(Box_user.Text,Box_pass.Password);
+                MessageBox.Show("Добро пожаловать в книжный магазин","Авторизация",MessageBoxButton.OK,MessageBoxImage.Information);
+                //LogWindow.Close(); //Скрытие окна авторизации
+            }
+            catch (SqliteException exeption)
+            {
+                MessageBox.Show(exeption.Message,"Error",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
         }
     }
 }
