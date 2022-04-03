72e4a39ca66f6fe319671001f93cdbcc8a4cae76
ArtBuz <buzinovart@yandex.ru>
2022-04-03 19:53:14 +0300

    connect Reg method to GUI

--- Bookstore.GUI/Regisrty.xaml.cs
+++ Bookstore.GUI/Regisrty.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Bookstrore.MControl.Control;
 
 
 namespace Bookstore.GUI
@@ -25,6 +26,33 @@ namespace Bookstore.GUI
             InitializeComponent();
         }
 
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            string name, pass, mail;
+            if (String.IsNullOrWhiteSpace(TextBlock_Name.Text.ToString()) || String.IsNullOrWhiteSpace(TextBlock_Pass.Text.ToString()) || String.IsNullOrWhiteSpace(TextBlock_Mail.Text.ToString()))
+            {
+                MessageBox.Show("Введены не корректные данные", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else
+            {
+                name = (TextBlock_Name.Text.ToString().Replace(" ", ""));
+                pass = (TextBlock_Pass.Text.ToString().Replace(" ", ""));
+                mail = (TextBlock_Mail.Text.ToString().Replace(" ", ""));
+            }
+
+            try
+            {
+                AccountManager.Registration(name, pass, mail);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        //Зачем нужны эти методы?
         private void tb1_TextChanged(object sender, TextChangedEventArgs e)
         {
 
@@ -39,5 +67,7 @@ namespace Bookstore.GUI
         {
 
         }
+        //
+
     }
 }
