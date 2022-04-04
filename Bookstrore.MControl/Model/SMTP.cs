4f3833e7897b1cd78eab8019d2911b62b15f94ee
ArtBuz <buzinovart@yandex.ru>
2022-04-04 21:02:59 +0300

    add Base class structure

---
+++ Bookstrore.MControl/Model/SMTP.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstrore.MControl.Model
+{
+    public class SMTP
+    {
+        //переменная - адрес отправителя
+        private MailAddress _sender = new MailAddress("Bookstore.Net@yandex.ru","Книжный магазин .NET");
+        
+        //переменная - адрес получателя
+        private readonly MailAddress _resipient;
+
+        //TODO: Может быть стоит использовать enum?
+        private readonly MailMessage _message;
+
+        private enum _messageType
+        {
+            
+        }
+
+    }
+}
