1fe38b1cfa93c7eafa92bb12962c2648a8cc6269
Kasady <pupcok@yandex.ru>
2022-04-07 11:46:52 +0300

    add  MainPage

---
+++ Bookstore.GUI/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bookstore.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Window
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_catalog(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_cart(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_persona(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
