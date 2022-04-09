f24f1468d73c97516204fb71c1c40f8d8d520c5f
Artem Buzinov <buzinovart@yandex.ru>
2022-04-09 13:29:51 +0300

    add class User

---
+++ Bookstrore.MControl/Model/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bookstore.MControl;
+
+namespace Bookstrore.MControl.Model
+{
+    internal class User
+    {
+        private BookstoreDb _db;
+        private SMTP _smtp;
+        private string _log;
+
+        public BookstoreDb DB
+        {
+            get { return _db;}
+            set { _db = value; }
+        }
+
+        public string Log
+        {
+            get { return _log;}
+            set { _log = value; }
+        }
+    }
+}
