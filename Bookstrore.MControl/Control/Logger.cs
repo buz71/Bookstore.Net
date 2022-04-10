794c5e86e4d2668f96f3698074eeebc669dbeaac
ArtBuz <buzinovart@yandex.ru>
2022-04-10 16:37:55 +0300

    Добавлены асинхронные методы Создания и загрузки файла логов

--- Bookstrore.MControl/Control/Logger.cs
+++ Bookstrore.MControl/Control/Logger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,32 @@ namespace Bookstrore.MControl.Control
 {
     public static class Logger
     {
+        private static void CreateFile(string accName)
+        {
+            File.CreateText($"{accName}.txt");
+        }
 
+        private static FileStream LoadFile(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                return new FileStream(fileName,FileMode.Open);
+            }
+            else
+            {
+                throw new FileLoadException("Файл не найден");
+            }
+
+        }
+
+        public static async void CreateLog(string accName)
+        {
+            await Task.Run(() => CreateFile(accName));
+        }
+
+        public static async Task<FileStream> LoadLog(string fileName)
+        {
+           return await Task.Run(() => LoadFile(fileName));
+        }
     }
 }
