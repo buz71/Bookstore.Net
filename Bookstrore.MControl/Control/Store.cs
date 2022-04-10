7f6eccde66446e9cdae2e8cad33dd4649d3072a8
ArtBuz <buzinovart@yandex.ru>
2022-04-10 20:50:18 +0300

    Создал статический класс Store

---
+++ Bookstrore.MControl/Control/Store.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstrore.MControl.Control
+{
+    /// <summary>
+    /// Класс для управления магазином
+    /// </summary>
+    public static class Store
+    {
+
+    }
+}
